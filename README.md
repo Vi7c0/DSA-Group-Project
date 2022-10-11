f1b086a456193233f746d73db68e62317093e697
Aiden.J <kiddow12345@gmail.com>
2022-10-11 10:36:27 +0200

    Created src and Main class

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # DSA Group Project
 This is a project to create a music player app
-hello
---
+++ src/src/MPlayer.java
@@ -0,0 +1,5 @@
+public class MPlayer {
+    public static void main(String[] args) {
+        System.out.println("This is a music player");
+    }
+}
